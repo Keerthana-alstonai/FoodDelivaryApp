26d876cf776c97768fc1e5bc4dafbc9fbd13a576
Keerthana K S <keerthanakeerthiks21@gmail.com>
2026-02-12 11:38:06 +0530

    hii is added

--- Hello.java
+++ Hello.java
@@ -2,3 +2,8 @@ class Hello{
 public static void main(String[] args){
 System.out.println("hello is displayed");
 }}
+
+public class Hello{
+public static void main(String[]args){
+System.out.println("Hii is displayed");
+}}
